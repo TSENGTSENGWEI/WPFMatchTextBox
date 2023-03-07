16c6443d6baa50a6414022995ddf79922117a492
TSENGTSENGWEI <66509991+TSENGTSENGWEI@users.noreply.github.com>
2023-03-07 16:36:43 +0800

    加入專案檔案。

---
+++ WpfApp4/MatchTextBox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfApp4
+{
+    internal class MatchTextBox: TextBox
+    {        
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            var Pattern = $"^(\\-|\\+)?\\d+(\\.\\d+)?$";
+            var PatternEndPoint = $"^(\\-|\\+)?\\d+(\\.){{1}}$";// (-)XX. 情況
+            if (this.Text != "-")
+            {
+                if(!Regex.IsMatch(this.Text, Pattern))
+                {
+                    if (Regex.IsMatch(this.Text, PatternEndPoint))
+                    {
+                        return;
+                    }
+                    this.Text = "";
+                }
+            }
+           
+        }
+    }
+}
